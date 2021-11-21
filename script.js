64db6c7e09a84c7c985bd54f7e5bfee62a11b4a4
Gandi Purna Jen <nyonggpj9995@gmail.com>
2021-11-21 15:19:22 +0900

    Create display html css

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let cekSubmit = document.getElementById("formInput"),
+cekValue = document.getElementById("addTask"),
+sendResult = document.getElementById("showResult"),
+myList = ["olahraga", "ngopi", "ngoding"];;
